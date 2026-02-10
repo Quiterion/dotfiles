e064966ef2a93ce3a8cbc3a53124aade61182650
Quiterion <46546618+Quiterion@users.noreply.github.com>
2026-02-10 15:26:44 +1100

    misc: fix waybar spacing

--- .config/waybar/layout.sh
+++ .config/waybar/layout.sh
@@ -3,15 +3,20 @@
 swaymsg -t subscribe -m '[ "window", "binding" ]' | while read line; do
     layout=$(swaymsg -t get_tree | jq 'recurse(.nodes[]) | select(.nodes[].focused? == true) | .layout')
     case $layout in
-        '"splith"')
-            echo "/ ";;
-        '"splitv"')
-            echo "/ ";;
-        '"tabbed"')
-            echo "/ 󰓩";;
-        '"stacked"')
-            echo "/ ";;
-        *)
-            echo "";;
+    '"splith"')
+        echo ""
+        ;;
+    '"splitv"')
+        echo ""
+        ;;
+    '"tabbed"')
+        echo "󰓩"
+        ;;
+    '"stacked"')
+        echo ""
+        ;;
+    *)
+        echo ""
+        ;;
     esac
 done
